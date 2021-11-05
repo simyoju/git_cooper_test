5d9e53fe99259563757f71b93ff0814491d630ed
Mijeong Jeon <ninevincentg@gmail.com>
2021-11-05 10:11:26 +0900

    Initial Commit

---
+++ NavigationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NavigationApp
+//
+//  Created by Mijeong 🌿 on 2021/11/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
