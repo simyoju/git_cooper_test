e2791f817b08a4d80273e901758d93fd485a4bf0
chpark6044 <chpark6044@naver.com>
2021-11-05 16:25:25 +0900

    update label

--- NavigationApp/FirstViewController.swift
+++ NavigationApp/FirstViewController.swift
@@ -14,7 +14,7 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        nameLabel.text = "청주"
+        nameLabel.text = "고생하셨습니다"
         // Do any additional setup after loading the view.
     }
 
