96c8a791ae9fb862a9e6349f04bc140b8128fdb5
simyoju <dawnofspring@daum.net>
2021-11-05 15:26:20 +0900

    modify second screen

--- NavigationApp/SecondViewController.swift
+++ NavigationApp/SecondViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 class SecondViewController: UIViewController {
 
     @IBOutlet weak var nameLabel: UILabel!
-
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        nameLabel.text = "청주의 주"
+        imageView.image = UIImage(named: "github")
 
         // Do any additional setup after loading the view.
     }
